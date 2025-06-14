a78ae475b9abedce86b9cd5ab123ab72e400b1d7
Sayan Barma <sayanbarma2004@gmail.com>
2025-06-15 01:16:44 +0530

    deployement

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: import.meta.env.MODE === "production" ? "/portfolio/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
   resolve: {
     alias: {
       "@": "/src",
